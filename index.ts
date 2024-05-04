b4637b704f54e6b6360187af53841c6486d88757
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-05-04 12:20:46 -0400

    chore: Add URL shortening functionality with random ID generation

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+/* random number generator for picking random letters */
+import { randomInt } from 'crypto';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
@@ -7,10 +10,30 @@ const port = 3000;
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 var storedURLs: string[] = [];
+
+function generateID(): string {
+    const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers: string = '0123456789';
+    const characters: string = alphabet + alphabet.toUpperCase() + numbers;
+    let id: string = '';
+    for (let i = 0; i < 6; i++) {
+        id += characters[randomInt(0, characters.length)];
+    }
+    return id;
+}
  
 function addURL(url: string) {
-    storedURLs.push(url);
+    const id = generateID();
+    console.log(`Shortening ${url} to ${id}`);
+    var shortURL = `http://localhost:${port}/${id}`;
+    storedURLs.push(shortURL);
+    return shortURL;
 }
 
 function isValidURL(url: string) {
@@ -25,6 +48,19 @@ function isValidURL(url: string) {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.post('/api/shorten', jsonParser, (req, res) => {
+    var url = req.body.url as string;
+    if (!url) {
+        res.status(400).send('Missing URL parameter');
+        console.log('Missing URL parameter');
+        return;
+    }
+    var url_ = addURL(url);
+    res.status(200).send({ url: url_ });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
